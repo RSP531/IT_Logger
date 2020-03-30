81f531fab6dace1948c6ba2a5ec77179d529d332
Rob Peschke <rspesc31@gmail.com>
2020-03-30 12:20:45 -0400

    added tech list modal and tech list item

--- it_logger/src/App.js
+++ it_logger/src/App.js
@@ -6,6 +6,9 @@ import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
 import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
 
 const App = () => {
   useEffect(() => {
@@ -17,8 +20,11 @@ const App = () => {
       <SearchBar />
       <div className="container">
         <AddBtn />
-        <Logs />
         <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
+        <Logs />
       </div>
     </Fragment>
   );
