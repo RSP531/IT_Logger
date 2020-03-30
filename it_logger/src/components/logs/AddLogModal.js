88d0ac58e9fbec0dc148d003974f886baebdb5dd
Rob Peschke <rspesc31@gmail.com>
2020-03-30 11:59:14 -0400

    added toast

--- it_logger/src/components/logs/AddLogModal.js
+++ it_logger/src/components/logs/AddLogModal.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import M from "materialize-css/dist/js/materialize.min.js";
 
 const AddLogModal = () => {
   const [message, setMessage] = useState("");
@@ -6,6 +7,9 @@ const AddLogModal = () => {
   const [tech, setTech] = useState("");
 
   const onSubmit = () => {
+    if (message === "" || tech === "") {
+      M.toast({ html: "Please enter a message and a tech" });
+    }
     console.log(message, tech, attention);
   };
 
