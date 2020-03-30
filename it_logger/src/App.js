b88c3394a75bee9405fa470b88fee1104fdd220f
Rob Peschke <rspesc31@gmail.com>
2020-03-30 10:46:11 -0400

    added logs

--- it_logger/src/App.js
+++ it_logger/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import SearchBar from "./components/layout/SearchBar";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -10,10 +10,9 @@ const App = () => {
     M.AutoInit();
   });
   return (
-    <div className="App">
+    <Fragment>
       <SearchBar />
-      Rob's App
-    </div>
+    </Fragment>
   );
 };
 
