d5b7da1ce49129bac78cfbc2983d8b5b12cc57b3
Rob Peschke <rspesc31@gmail.com>
2020-03-31 10:33:38 -0400

    refactored log actions and getlogs

--- it_logger/src/actions/logActions.js
+++ it_logger/src/actions/logActions.js
@@ -1,8 +1,9 @@
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 
-export const getLogs = () => {
-  return async dispatch => {
+export const getLogs = () => async dispatch => {
+  try {
     setLoading();
+
     const res = await fetch("./logs");
     const data = await res.json();
 
@@ -10,7 +11,12 @@ export const getLogs = () => {
       type: GET_LOGS,
       payload: data
     });
-  };
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
 };
 
 export const setLoading = () => {
