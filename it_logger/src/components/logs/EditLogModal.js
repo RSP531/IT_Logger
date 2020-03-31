b98a82ac5f70c092e5831a32df2d9a46ba85ea08
Rob Peschke <rspesc31@gmail.com>
2020-03-31 17:11:53 -0400

    added the edit log modal

--- it_logger/src/components/logs/EditLogModal.js
+++ it_logger/src/components/logs/EditLogModal.js
@@ -21,7 +21,17 @@ const EditLogModal = ({ current, updateLog }) => {
     if (message === "" || tech === "") {
       M.toast({ html: "Please enter a message and tech" });
     } else {
-      console.log(message, tech, attention);
+      const updLog = {
+        id: current.id,
+        message,
+        attention,
+        tech,
+        date: new Date()
+      };
+
+      updateLog(updLog);
+      M.toast({ html: `Log updated by ${tech}` });
+
       setMessage("");
       setTech("");
       setAttention(false);
@@ -40,9 +50,6 @@ const EditLogModal = ({ current, updateLog }) => {
               value={message}
               onChange={e => setMessage(e.target.value)}
             />
-            <label htmlFor="message" className="active">
-              Log Message
-            </label>
           </div>
         </div>
         <div className="row">
