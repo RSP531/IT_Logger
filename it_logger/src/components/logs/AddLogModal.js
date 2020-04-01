5496154ec1fdbcef68268a3b892024df3800e1a6
Rob Peschke <rspesc31@gmail.com>
2020-04-01 10:29:13 -0400

    refactored so the selector list is from the database json file

--- it_logger/src/components/logs/AddLogModal.js
+++ it_logger/src/components/logs/AddLogModal.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { addLog } from "../../actions/logActions";
 import M from "materialize-css/dist/js/materialize.min.js";
+import TechSelectOptions from "../techs/TechSelectOptions";
 
 const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState("");
@@ -59,9 +60,7 @@ const AddLogModal = ({ addLog }) => {
                 {" "}
                 Select Technician
               </option>
-              <option value="John Doe">John Doe</option>
-              <option value="Sam Smith">Sam Smith</option>
-              <option value="Sarah Wilson">Sarah Wilson</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
